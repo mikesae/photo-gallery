4e44de270a3cb6db813766c5d62e70daf4901464
mikesae <msaelens@gmail.com>
2024-03-06 11:46:54 -0500

    Use cards for albums component too; Add route back to it.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { AlbumComponent } from './album/album.component'
+import { AlbumsComponent } from './albums/albums.component';
 
 const routes: Routes = [
-  { path: 'album/:id', component: AlbumComponent }
+  { path: '', component: AlbumsComponent},
+  { path: 'album/:id', component: AlbumComponent },
+  { path: '**', component: AlbumsComponent}
 ];
 
 @NgModule({
